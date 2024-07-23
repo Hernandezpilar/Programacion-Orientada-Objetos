a1630305af22e928ab7e632a38b653805e9c3a68
Hernandezpilar <cuh51239922@cuh.mx>
2024-07-23 17:21:20 -0600

    Materiales.java

---
+++ Materiales.java
@@ -0,0 +1,10 @@
+package mx.com.cuh;
+
+public interface Materiales {
+
+	public void mostrarNombre();
+	public void mostrarExistencias();
+	public boolean hacerPedido();
+	public void mostrarPercio();
+	
+}
