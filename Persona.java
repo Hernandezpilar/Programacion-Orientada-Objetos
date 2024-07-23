8b421984d5a67cdb3c4a844d70f8e523c16b601b
Hernandezpilar <cuh51239922@cuh.mx>
2024-07-23 17:11:16 -0600

    Persona.java

--- Persona.java
+++ Persona.java
@@ -1,44 +1,54 @@
 package mx.com.cuh;
 
 public class Persona {
-	
-	private short altura = 0;
+
+	//Se inicializan los atributos de la clase
 	private String nombre = "";
+	private short altura = 0;
 	private short edad = 0;
+	private double sueldo = 0;
+	private boolean trabajando = false;
 	
 	
-	//se define constructor Persona con sus atributos principales al momento de instanciar
-	 public Persona(String nombre, short altura, short edad) {
-		 this.nombre = nombre;
-		 this.edad = edad;
-		 this.altura = altura;
-	 }
-	
-	
-	 
-	public short getAltura() {
-		return altura;
-	}
-	public void setAltura(short altura) {
+	//Se define el constructpr Persona con sus atributos principales al momento de instanciar
+	public Persona (String nombre, short altura, short edad, double sueldo) {
+		this.nombre = nombre;
 		this.altura = altura;
+		this.edad = edad;
+		this.sueldo = sueldo;
 	}
+
+	
+	//Se defina los gety set para la clase Persona
 	public String getNombre() {
 		nombre = "Sr." + nombre;
 		return nombre;
 	}
+	
 	public void mostrarNombre() {
 		System.out.println(this.nombre);
 	}
-	
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public short getEdad() {
-		return edad;
-	}
-	public void setEdad(short edad) {
-		this.edad = edad;
+
+	public String trabajar() {
+		trabajando = !trabajando;
+		String trabajo = "Trabajando";
+		return trabajo;
 	}
 	
+	public void cobrar() {
+		System.out.println("Cobrando");
+	}
+	
+	public void alimentarse() {
+		System.out.println("Hora de comida");
+	}
+	
+			
+	public void checar() {
+		if (!trabajando)
+			System.out.println("Ingreso registrado");
+		else
+			System.out.println("Salida registrada");
+	}
 	
 }
